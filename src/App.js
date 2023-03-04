769f9e509fff1cb32291c1550ad9f5673c3436fc
Trevor-Sr <rupialuanda@gmail.com>
2023-03-04 18:22:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Header } from './components/Header';
+import Heros from './components/Hero';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header/>
+      <Heros/>
+    </div>
+  );
+}
+
+export default App;

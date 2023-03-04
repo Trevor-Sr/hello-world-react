f8c7dc563890b50462b5bde27bb42286cc24b919
Trevor-Sr <rupialuanda@gmail.com>
2023-03-04 18:49:48 +0300

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export const Header = () => {
   return (
-    <div className=''>Header</div>
-  )
-}
-
- 
+    <>
+      <div className="">Header</div>
+      <div className="">Header</div>
+    </>
+  );
+};
